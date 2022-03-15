9964435120117768bc0467cc85ebcdb3df23e059
xuanco941 <xuanco941@gmail.com>
2022-03-15 21:47:26 +0700

    Sobel

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Áp Dụng Cho Ma Trận 5x5 Nhé!!!");
+double[,] maTran = new double[5, 5];
+Console.WriteLine("Nhập ma trận: ");
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.WriteLine($"Điểm ({i},{j}): ");
+        var x = Console.ReadLine();
+        maTran[i, j] = Convert.ToDouble(x);
+    }
+}
+Console.WriteLine();
+
+Console.WriteLine("Ma trận của bạn là: ");
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write($"{maTran[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Chọn loại thuật toán: ");
+System.Console.WriteLine("1.Sobel");
+System.Console.WriteLine("----------------------------------------------------");
+
+ThuatToan thuattoan = new ThuatToan();
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        thuattoan.Sobel(maTran);
+        break;
+
+}
